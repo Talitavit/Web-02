6e717e0bfd02cf3ff7a25d5e1fdc86ba8de1918b
Talitavit <alunoifpe@CIGR-266221.ifpe.edu>
2024-11-27 17:24:09 -0300

    atividade_5

---
+++ atividade_5/app/Models/Book.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Book extends Model
+{
+    use Hasfactory;
+
+    protected $fillade = ['title','author-id','category_id','publisher_id','publisher_year'];
+
+    public function author(){
+        return $this->belongsTo(Author::class);
+    }
+
+    public function category(){
+        return $this->beloongsTo(Category::class);
+    }
+
+    public function publisher (){
+        return $this->belongsTo(Publisher::class);
+
+    }
+
+    public function users(){
+        return $this->belongsToMany(Users::clas,'borrowings')
+        ->withPivot('borrowed_at','returned_at')
+        ->withTimestamps();
+    }
+
+
+
+}
